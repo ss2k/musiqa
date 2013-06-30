ff229eb9ec2c44a3e034b684a4f1dcd7daf72ea1
SS <git@push.com>
2013-06-29 23:15:40 -0700

    First Commit: Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musiqa::Application.config.secret_token = '0e3d9f7395afa7fbc01ee101a7909c678428dcd436fe5e6dd25e6fce8afedd1de370d693c0568420b5746c75ade37203e47ac65e68d84cf5d93e2ed218ebe03b'
