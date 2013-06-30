4076d5e14e158d532ffdcc05045a88f4c4728929
SS <git@push.com>
2013-06-30 11:03:17 -0700

    api stuff

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,24 @@
+#require 'net/http'
+require 'json'
 module QuestionsHelper
+  def automated?(question)
+    if question.similar
+      return true
+    elsif question.learn
+      return true
+    elsif !question.artist.nil?
+      return true
+    elsif !question.song.nil?
+      return true
+    else
+      return false
+    end
+  end
+
+  def find_similar_artists(artist)
+    api = ENV['en_api_key']
+    url = URI.parse('http://developer.echonest.com/api/v4/artist/similar?api_key=#{api}&name=#{artist}')
+    req = Net::HTTP::Get.new(url.to_s)
+    res = 
+  end
 end
